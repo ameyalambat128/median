f79717fb3e3744ab44838e1bc36d949a3fc45635
Ameya Lambat <ameyalambat@gmail.com>
2021-12-30 10:58:44 -0700

    feat: added styles to make the avatar and the header responsive

---
+++ pages/components/ChangeUsername.jsx
@@ -0,0 +1,9 @@
+const ChangeUsername = () => {
+  return (
+    <div className="text-sm absolute top-5 right-5">
+      <button className=""></button>
+    </div>
+  );
+};
+
+export default ChangeUsername;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
+
 import Login from "./components/Login";
 import { useMoralis } from "react-moralis";
+import Header from "./components/Header";
 
 export default function Home() {
   const { isAuthenticated, logout } = useMoralis();
@@ -9,13 +11,19 @@ export default function Home() {
     return <Login />;
   }
   return (
-    <div className="h-screen">
+    <div className="h-screen bg-gradient-to-b from-red-300 to-white overflow-hidden">
       <Head>
         <title>Dapp Chat</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome to Median</h1>
-      <button onClick={logout} className="">
+
+      <div className="h-screen overflow-y-scroll">
+        <div className="max-w-screen-2xl mx-auto">
+          <Header />
+          {/* <Messages /> */}
+        </div>
+      </div>
+      <button onClick={logout} className="p-10 bg-white z-50">
         Logout
       </button>
     </div>
