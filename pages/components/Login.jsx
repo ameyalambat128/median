1b8dd33d2e1215078c93d822be11603385b62144
Ameya Lambat <ameyalambat@gmail.com>
2021-12-30 10:54:19 -0700

    fix: increased the quality of the logo and corrected the dimensions

--- pages/components/Login.jsx
+++ pages/components/Login.jsx
@@ -2,18 +2,28 @@ import Image from "next/image";
 import background from "../../assets/bg.jpg";
 import logo from "../../assets/logo.png";
 import { useMoralis } from "react-moralis";
+import React, { useState } from "react";
 
 const Login = () => {
   const { authenticate } = useMoralis();
-
+  const [page, setPage] = useState();
   return (
     <div className="bg-black relative">
-      <h1 className="">This is the login page</h1>
-      <div className="flex flex-col w-full absolute items-center justify-center z-50 h-4/6 space-y-8">
-        <Image className="" src={logo} alt="logo" height={100} width={100} />
+      <div className="flex flex-col w-full absolute items-center justify-center z-50 h-4/6 space-y-4">
+        <Image
+          className=""
+          src={logo}
+          alt="logo"
+          layout="intrinsic"
+          objectFit="contain"
+          height={150}
+          width={200}
+          quality={100}
+        />
+
         <button
           onClick={authenticate}
-          className="bg-red-300 p-4 font-sans font-medium rounded-lg shadow-lg hover:brightness-110"
+          className="bg-red-300 p-4 font-sans font-medium rounded-lg shadow-lg hover:brightness-110 transition-all ease-in-out hover:px-10 "
         >
           Login to MEDIAN
         </button>
