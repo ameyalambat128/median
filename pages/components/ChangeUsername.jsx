533947766d1037a361b6677306c012e8bc45cb05
Ameya Lambat <ameyalambat@gmail.com>
2021-12-30 18:22:54 -0700

    feat: add the change username feature

--- pages/components/ChangeUsername.jsx
+++ pages/components/ChangeUsername.jsx
@@ -1,7 +1,25 @@
+import { useMoralis } from "react-moralis";
+import React, { useState } from "react";
+
 const ChangeUsername = () => {
+  const { setUserData, isUserUpdating, userError, user } = useMoralis();
+
+  const setUsername = () => {
+    const username = prompt(`Enter your Username: ${user.getUsername()}`);
+    if (!username) return;
+    setUserData({ username });
+  };
   return (
-    <div className="text-sm absolute top-5 right-5">
-      <button className=""></button>
+    <div className="">
+      <div className="text-sm absolute top-5 right-10">
+        <button
+          disabled={isUserUpdating}
+          onClick={setUsername}
+          className="text-black font-semibold rounded-lg border-2 bg-white border-white p-2  hover:shadow-lg hover:text-red-400"
+        >
+          Change your Username
+        </button>
+      </div>
     </div>
   );
 };
